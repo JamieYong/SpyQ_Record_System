79232053e513e0b0bd80fb23d63279d6187599ef
seojinsarejj <s19009@naver.com>
2020-01-12 00:51:33 +0900

    connect DB

--- SpyQ_Record_System/Form1.cs
+++ SpyQ_Record_System/Form1.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.Drawing;  
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace SpyQ_Record_System
 {
     public partial class Form1 : Form
     {
+
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=recordproject;Integrated Security=True");
         public Form1()
         {
             Console.WriteLine("Hello windowforms");
             InitializeComponent();
         }
+
     }
 }
+ 
