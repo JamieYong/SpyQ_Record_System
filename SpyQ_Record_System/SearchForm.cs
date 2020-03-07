33c9f3acea181fcce13198654115b5106f05ce10
seojinsarejj <s19009@naver.com>
2020-03-07 15:30:48 +0900

    데이터그리드뷰 새로고침 함수 생성

--- SpyQ_Record_System/SearchForm.cs
+++ SpyQ_Record_System/SearchForm.cs
@@ -18,71 +18,15 @@ namespace SpyQ_Record_System
         SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=recordproject;Integrated Security=True");
         public SearchForm()
         {
-            Console.WriteLine("Hello windowforms");
             InitializeComponent();
         }
 
-
-
-
-        private void TeamCBbox_Click(object sender, EventArgs e)
+        
+        
+        void refresh_RecordTable()
         {
-            
-                ArrayList arr = new ArrayList();
-                arr.Add("");
-                con.Open();
 
-                string query = String.Format("exec Pteamname '{0}', '{1}'", startdate.Text, enddate.Text);
-                SqlCommand cmd = new SqlCommand();
-                cmd.Connection = con;
-                cmd.CommandText = query;
-
-                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-                
-                con.Close();
-                DataSet ds = new DataSet();
-                adapter.Fill(ds);
-                DataTable dt = ds.Tables[0];
-
-                foreach (DataRow row in dt.Rows)
-                {
-                    arr.Add(row["TeamName"].ToString());
-                }
-
-
-
-
-                TeamCBbox.DataSource = arr;
-            
-
-        }
-
-        private void select_btn_Click(object sender, EventArgs e)
-        {
-            con.Open();
-
-
-     
-            string query = String.Format("exec Pselect @PdateA = '{0}' , @PdateB = '{1}' , @Pteam='{2}', @Pplayer ='{3}' ", startdate.Text, enddate.Text, TeamCBbox.Text, PlayerCBbox.Text);
-
-
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = con;
-            cmd.CommandText = query;
-
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            DataSet ds = new DataSet();
-           
-            da.Fill(ds, "record");
-
-            RecordTable.DataSource = ds;
-            RecordTable.DataMember = "record";
-
-
-
-
-
-                if (ColumnSelect.Text == "공격")
+            if (ColumnSelect.Text == "공격")
             {
 
                 for (int i = 3; i < 15; i++)
@@ -126,6 +70,62 @@ namespace SpyQ_Record_System
             }
 
 
+        }
+
+
+        private void TeamCBbox_Click(object sender, EventArgs e)
+        {
+            
+                ArrayList arr = new ArrayList();
+                arr.Add("");
+                con.Open();
+                string query = String.Format("exec Pteamname '{0}', '{1}'", startdate.Text, enddate.Text);
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = query;
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                
+                con.Close();
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                DataTable dt = ds.Tables[0];
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    arr.Add(row["TeamName"].ToString());
+                }
+
+
+
+
+                TeamCBbox.DataSource = arr;
+            
+
+        }
+
+        private void select_btn_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            string query = String.Format("exec Pselect @PdateA = '{0}' , @PdateB = '{1}' , @Pteam='{2}', @Pplayer ='{3}' ", startdate.Text, enddate.Text, TeamCBbox.Text, PlayerCBbox.Text);
+
+
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = query;
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+           
+            da.Fill(ds, "record");
+
+            RecordTable.DataSource = ds;
+            RecordTable.DataMember = "record";
+
+
+            refresh_RecordTable();
 
 
             con.Close();
@@ -138,6 +138,7 @@ namespace SpyQ_Record_System
             ArrayList arr = new ArrayList();
             arr.Add("");
             con.Open();
+
             string query = String.Format("exec Pplayername '{0}'",TeamCBbox.Text);
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = con;
@@ -174,55 +175,9 @@ namespace SpyQ_Record_System
 
         private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
         {
-            try
-            {
-                if (ColumnSelect.Text == "공격")
-                {
-
-                    for (int i = 3; i < 15; i++)
-                    {
-
-                        if (i >= 8)
-                        {
-                            RecordTable.Columns[i].Visible = false;
-                        }
-                        else
-                        {
-                            RecordTable.Columns[i].Visible = true;
-                        }
-
-                    }
 
 
-                }
-                else if (ColumnSelect.Text == "수비")
-                {
-                    for (int i = 3; i < 15; i++)
-                    {
-
-                        if (i < 8)
-                        {
-                            RecordTable.Columns[i].Visible = false;
-                        }
-                        else
-                        {
-                            RecordTable.Columns[i].Visible = true;
-                        }
-                    }
-
-                }
-                else
-                {
-                    for (int i = 0; i <= 15; i++)
-                    {
-                        RecordTable.Columns[i].Visible = true;
-                    }
-                }
-
-            }catch
-            {
-
-            }
+            refresh_RecordTable();
 
 
 
